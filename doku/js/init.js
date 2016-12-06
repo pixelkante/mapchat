d9c541f42e351fcbea7edcf36638846378ee6cb7
Sebastian <s.oley@me.com>
2016-12-06 17:53:34 +0100

    doku base finished

--- doku/js/init.js
+++ doku/js/init.js
@@ -2,10 +2,25 @@
  * Created by Sebastian Oley on 05.11.2016.
  */
 
-var categories = document.getElementsByClassName('category');
-var nav = document.createElement('nav');
+function addContent() {
+    var pages = document.getElementsByClassName('page');
+
+    for (var i = 0; i < pages.length; i++) {
+        var path = 'content/' + pages[i].getAttribute('data-path');
+        $(".page").eq(i).load(path);    // ajax
+    }
+}
 
 function addNav() {
+    var categories = document.getElementsByClassName('category');
+    var nav = document.createElement('nav');
+
+    var navHead = document.createElement('a');
+    navHead.innerHTML = '<h1>' + document.getElementsByTagName('header')[0].getAttribute('data-name') + '</h1>';
+    navHead.href = '#top';
+    document.getElementsByTagName('header')[0].id  = 'top';
+    nav.appendChild(navHead);
+
     for (var i = 0; i < categories.length; i++) {
         // add category headings and list
         var categoryHead = document.createElement('h4');
@@ -22,21 +37,16 @@ function addNav() {
                                     // e.g. '<li>1.1 xyz</li>'
             nav.getElementsByTagName('ul')[i].appendChild(subcategory);
 
-            // add anker to referred page
-            var anker = document.createElement('a');
-            anker.name = (i + 1) + '-' + (k + 1);
-            categories[i].insertBefore(anker, pages[k]);
+            // add anchor to referred page
+            pages[k].id = (i + 1) + '-' + (k + 1);
+
+            // add heading to referred page
+            var heading = document.createElement('div');
+            heading.className = 'page-heading';
+            heading.innerHTML = (i + 1) + '.' + (k + 1) + ' ' + pages[k].getAttribute('data-name');
+            categories[i].insertBefore(heading, pages[k]);
         }
     }
 
     document.body.appendChild(nav);
-}
-
-function addContent() {
-    var pages = document.getElementsByClassName('page');
-
-    for (var i = 0; i < pages.length; i++) {
-        var path = 'content/' + pages[i].getAttribute('data-path');
-        $(".page").eq(i).load(path);    // ajax
-    }
 }
